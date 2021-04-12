fe52a98433a5f3727a2b2c36077d5d2bd41e8e30
kmranrg <kmranrg@gmail.com>
2021-04-12 15:47:02 +0530

    stick to portrait mode only

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:avatar_glow/avatar_glow.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'home_page.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
